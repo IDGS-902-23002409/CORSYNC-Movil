098bf12fd74c320293db1b3d993ae11b818cde81
saul-greyna <saulreynag.07@gmail.com>
2026-07-21 22:43:50 -0600

    Primera version de la integracion del modulo Unity unicamente con Auras

---
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,137 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FireAnimation::Start()
+extern void FireAnimation_Start_m2DA36DC7CD271C615942DE6CA022434DACB10E35 (void);
+// 0x00000002 System.Void FireAnimation::Update()
+extern void FireAnimation_Update_mCBE62CEA6210297D605742DC32E16A86CA361B96 (void);
+// 0x00000003 System.Void FireAnimation::.ctor()
+extern void FireAnimation__ctor_m88809DFAEC02A9DB32EC6FDB59E1301E9E68EE32 (void);
+// 0x00000004 System.Void RutaVuelo::Start()
+extern void RutaVuelo_Start_m95605CEDC61869E6B9992989F7714B4F0370AD57 (void);
+// 0x00000005 System.Void RutaVuelo::Update()
+extern void RutaVuelo_Update_m0177267791F4BBDA8ED860B3CFE8FD1A7F9B4B7D (void);
+// 0x00000006 System.Void RutaVuelo::.ctor()
+extern void RutaVuelo__ctor_m83101889252A6B89D6B7116DE8C083862E75DF1B (void);
+// 0x00000007 System.Void RutaVuelo2::Start()
+extern void RutaVuelo2_Start_mDBC4172CC088D6B9DE3E48CFA09B87CD6F61312F (void);
+// 0x00000008 System.Void RutaVuelo2::Update()
+extern void RutaVuelo2_Update_mFC3A3458F56A91E50619A0FFDC796E3D381A5E71 (void);
+// 0x00000009 System.Void RutaVuelo2::.ctor()
+extern void RutaVuelo2__ctor_mDF5D7F91119B165B662B6C26E6D4166283149976 (void);
+// 0x0000000A System.Void EpicToonFX.ETFXLoopScript::Start()
+extern void ETFXLoopScript_Start_m8B9F608C71269FF287FF5CF6A0D12F728446FE59 (void);
+// 0x0000000B System.Void EpicToonFX.ETFXLoopScript::Update()
+extern void ETFXLoopScript_Update_m1F41B84A3623BE28B3910D5BBC7BE017D59ADC9A (void);
+// 0x0000000C System.Void EpicToonFX.ETFXLoopScript::PlayEffect()
+extern void ETFXLoopScript_PlayEffect_mB0F1246770022F592F854F71DA7E1A13C3949DCB (void);
+// 0x0000000D System.Collections.IEnumerator EpicToonFX.ETFXLoopScript::EffectLoop()
+extern void ETFXLoopScript_EffectLoop_m9BD0D9DFE864FB2692165E94270E5CFB64CBE5BE (void);
+// 0x0000000E System.Void EpicToonFX.ETFXLoopScript::.ctor()
+extern void ETFXLoopScript__ctor_m7D358DD45F488706D4E3340A8540A6AF526BB6B7 (void);
+// 0x0000000F System.Void EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::.ctor(System.Int32)
+extern void U3CEffectLoopU3Ed__9__ctor_mFC2801E0F1BA7F78B550548F9A604CD3278BB602 (void);
+// 0x00000010 System.Void EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::System.IDisposable.Dispose()
+extern void U3CEffectLoopU3Ed__9_System_IDisposable_Dispose_mB6E728C5CF4EE7FBFBE15CB3C3396726AC6DC21F (void);
+// 0x00000011 System.Boolean EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::MoveNext()
+extern void U3CEffectLoopU3Ed__9_MoveNext_m70AFA5C0A5B36653154F92C831CB5D007811D855 (void);
+// 0x00000012 System.Object EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CEffectLoopU3Ed__9_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB1724D11CF1D3303C55A2C3F18B8E9D557F100FF (void);
+// 0x00000013 System.Void EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::System.Collections.IEnumerator.Reset()
+extern void U3CEffectLoopU3Ed__9_System_Collections_IEnumerator_Reset_mF0623CCAE8B6909B9DE7E5B5CC9BF22A8D5D48A9 (void);
+// 0x00000014 System.Object EpicToonFX.ETFXLoopScript/<EffectLoop>d__9::System.Collections.IEnumerator.get_Current()
+extern void U3CEffectLoopU3Ed__9_System_Collections_IEnumerator_get_Current_m753226AEDEA85CCB079E26CA39FB52C0A41888DB (void);
+// 0x00000015 System.Void EpicToonFX.ETFXMouseOrbit::Start()
+extern void ETFXMouseOrbit_Start_m883ED864D07ED2B013CBEC297E413AC4F7E8403A (void);
+// 0x00000016 System.Void EpicToonFX.ETFXMouseOrbit::LateUpdate()
+extern void ETFXMouseOrbit_LateUpdate_m88BAD2785B3447A239E1094CBEBEC160B1D5C270 (void);
+// 0x00000017 System.Single EpicToonFX.ETFXMouseOrbit::ClampAngle(System.Single,System.Single,System.Single)
+extern void ETFXMouseOrbit_ClampAngle_m0465F9A5E01116400F3D22596C9EFB68F949297D (void);
+// 0x00000018 System.Void EpicToonFX.ETFXMouseOrbit::.ctor()
+extern void ETFXMouseOrbit__ctor_mCCECFB7E06C2B4F0D1F93D142B8CA91738C63370 (void);
+static Il2CppMethodPointer s_methodPointers[24] = 
+{
+	FireAnimation_Start_m2DA36DC7CD271C615942DE6CA022434DACB10E35,
+	FireAnimation_Update_mCBE62CEA6210297D605742DC32E16A86CA361B96,
+	FireAnimation__ctor_m88809DFAEC02A9DB32EC6FDB59E1301E9E68EE32,
+	RutaVuelo_Start_m95605CEDC61869E6B9992989F7714B4F0370AD57,
+	RutaVuelo_Update_m0177267791F4BBDA8ED860B3CFE8FD1A7F9B4B7D,
+	RutaVuelo__ctor_m83101889252A6B89D6B7116DE8C083862E75DF1B,
+	RutaVuelo2_Start_mDBC4172CC088D6B9DE3E48CFA09B87CD6F61312F,
+	RutaVuelo2_Update_mFC3A3458F56A91E50619A0FFDC796E3D381A5E71,
+	RutaVuelo2__ctor_mDF5D7F91119B165B662B6C26E6D4166283149976,
+	ETFXLoopScript_Start_m8B9F608C71269FF287FF5CF6A0D12F728446FE59,
+	ETFXLoopScript_Update_m1F41B84A3623BE28B3910D5BBC7BE017D59ADC9A,
+	ETFXLoopScript_PlayEffect_mB0F1246770022F592F854F71DA7E1A13C3949DCB,
+	ETFXLoopScript_EffectLoop_m9BD0D9DFE864FB2692165E94270E5CFB64CBE5BE,
+	ETFXLoopScript__ctor_m7D358DD45F488706D4E3340A8540A6AF526BB6B7,
+	U3CEffectLoopU3Ed__9__ctor_mFC2801E0F1BA7F78B550548F9A604CD3278BB602,
+	U3CEffectLoopU3Ed__9_System_IDisposable_Dispose_mB6E728C5CF4EE7FBFBE15CB3C3396726AC6DC21F,
+	U3CEffectLoopU3Ed__9_MoveNext_m70AFA5C0A5B36653154F92C831CB5D007811D855,
+	U3CEffectLoopU3Ed__9_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB1724D11CF1D3303C55A2C3F18B8E9D557F100FF,
+	U3CEffectLoopU3Ed__9_System_Collections_IEnumerator_Reset_mF0623CCAE8B6909B9DE7E5B5CC9BF22A8D5D48A9,
+	U3CEffectLoopU3Ed__9_System_Collections_IEnumerator_get_Current_m753226AEDEA85CCB079E26CA39FB52C0A41888DB,
+	ETFXMouseOrbit_Start_m883ED864D07ED2B013CBEC297E413AC4F7E8403A,
+	ETFXMouseOrbit_LateUpdate_m88BAD2785B3447A239E1094CBEBEC160B1D5C270,
+	ETFXMouseOrbit_ClampAngle_m0465F9A5E01116400F3D22596C9EFB68F949297D,
+	ETFXMouseOrbit__ctor_mCCECFB7E06C2B4F0D1F93D142B8CA91738C63370,
+};
+static const int32_t s_InvokerIndices[24] = 
+{
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5637,
+	5530,
+	5637,
+	4421,
+	5637,
+	5440,
+	5530,
+	5637,
+	5530,
+	5637,
+	5637,
+	7009,
+	5637,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	24,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
